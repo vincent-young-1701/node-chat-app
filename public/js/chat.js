7e2314850ded36eed7790538348f904e772b0d17
Captain Kirk <captainkirk@Vincents-MacBook-Pro-2.local>
2018-09-09 17:01:29 -0700

    Added people list

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,6 +16,14 @@ socket.on('connect', function () {
 
 });
 
+socket.on('updateUserList', (users) => {
+  var ol = jQuery('<ol></ol>');
+  users.forEach(user => {
+    ol.append(jQuery('<li></li>').text(user));
+  });
+  jQuery('#users').html(ol);
+});
+
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
