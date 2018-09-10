c6d2caf4997905c923661f20533d0ea07ec29f34
Captain Kirk <captainkirk@Vincents-MacBook-Pro-2.local>
2018-09-09 17:28:08 -0700

    Bug fix

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ io.on('connection', (socket) => {
   socket.on('createMessage', (message, callback) => {
     var user = users.getUser(socket.id);
     if(user && isRealString(message.text)) {
-      io.to(params.room).emit('newMessage', generateMessage(user.name, message.text));
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
     }
     callback();
   });
